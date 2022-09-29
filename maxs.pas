2399719cdc81ff0f0b9d7b3c1201341dc67d7bb0
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 12:17:25 +0100

    user function

---
+++ maxs.pas
@@ -0,0 +1,21 @@
+Program main;
+
+function maxs(i,ii:integer): integer;
+var
+  retss:integer;
+  c:char;
+Begin
+      retss:=ii;
+      if i>ii then retss:=i;
+      
+      maxs:=ii;
+End;
+
+(* Here the main program block starts *)
+Begin
+
+    writeln( chr(27) +'[42;30m hello world\n]');  
+    writeln(maxs(20,40));
+
+End.
+
